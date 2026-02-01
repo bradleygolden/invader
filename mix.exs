ab248535dcd2e25658fefb59b9d018de072954e4
Bradley Golden <golden.bradley@gmail.com>
2026-02-01 08:19:03 -0600

    Add mix invader.gen.env task for local development setup

--- mix.exs
+++ mix.exs
@@ -105,7 +105,7 @@ defmodule Invader.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "invader.gen.env", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ash.setup --quiet", "test"],
